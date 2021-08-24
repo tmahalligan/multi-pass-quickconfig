fe5cb3ce264ea6a854a98b8c0f1c6ede4142002d
Thomas Halligan <tmhalligan@gmail.com>
2021-08-24 08:42:20 -0400

    Scope down carvel script

--- README.md
+++ README.md
@@ -12,24 +12,24 @@ Linux: https://multipass.run/docs/installing-on-linux
 ### Pull down config automation ###
 Create a local directory to pull the multipass_config repo and clone repo
 
-mkdir /projects/demo
+mkdir /Users/username/projects/demo
 
-cd /projects/demo
+cd /Users/username/projects/demo
 
 git clone https://github.com/tmahalligan/multi-pass-quickconfig.git
 
-### Launch Multipass instance ####
+### Launch Multipass instance with minikube ####
 Launch arguments
 -d (Disk space) -m (Memory) -c (cpu) -n (Name) and final argument is image version. Adjust according to your needs (For minikube to function 2 cpu must be allocated )
 
-multipass launch -d 10G -m 2G -c 2 -n utest 20.04
+multipass launch -d 40G -m 4G -c 3 -n utest minikube
 
 ### Mount the config automation (You must use full path)
-multipass mount /Users/thalligan/projects/demo/multipass_config utest
+multipass mount /Users/username/projects/demo/multi-pass-quickconfig utest
 
 ### Shell in and run automation (A number of tools will be installed run may take a number of minutes)
 multipass shell utest
-cd /Users/thalligan/projects/demo/multipass_config
+cd /Users/thalligan/projects/demo/multi-pass-quickconfig
 bash setup.sh. (This will take awhile)
 
 #### Exit shell and remove mount
---
+++ full.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo "Updating and Upgrading Linux Packages"
+sleep 5
+sudo apt-get -y update && 
+sudo apt-get -y install software-properties-common git python3-pip &&
+
+echo "Upgrading PIP and installing"
+sleep 5
+sudo pip3 install --upgrade pip &&
+
+echo "Install kubectl"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - &&
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list &&
+sudo apt-get update && sudo apt-get install -y kubectl &&
+
+echo "Remove any old Docker items"
+sudo apt remove docker docker-engine docker.io containerd runc &&
+
+echo "Install all pre-reqs for Docker"
+sleep 5
+sudo apt update &&
+sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common &&
+
+echo "Add the Docker repository, we are installing from Docker and not the Ubuntu APT repo."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
+sudo apt-key fingerprint 0EBFCD88 &&
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&
+
+echo "Install Docker"
+sleep 5
+sudo apt update &&
+sudo apt install -y docker-ce docker-ce-cli containerd.io &&
+
+echo "Add your base user to the Docker group so that you do not need sudo to run docker commands"
+sudo usermod -aG docker ubuntu &&
+
+echo "Install helm"
+sleep 5
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash &&
+
+echo "Install sops"
+sleep 5
+wget https://github.com/mozilla/sops/releases/download/v3.7.1/sops_3.7.1_amd64.deb &&
+sudo dpkg -i sops_3.7.1_amd64.deb &&
+sudo rm sops_3.7.1_amd64.deb &&
+
+echo "Install minikube"
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 &&
+sudo install minikube-linux-amd64 /usr/local/bin/minikube &&
+
+
+echo "Install k9s"
+sudo wget https://github.com/derailed/k9s/releases/download/v0.24.15/k9s_Linux_x86_64.tar.gz &&
+sudo tar -xvf k9s_Linux_x86_64.tar.gz &&
+sudo mv k9s /usr/local/bin/k9s &&
